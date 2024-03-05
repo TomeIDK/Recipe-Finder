ab3430e9d0163994099a8c36cf129cb1e4c262bd
tomeidk <cecepas2001@gmail.com>
2024-03-05 18:17:10 +0100

    Update preferences.js

--- js/preferences.js
+++ js/preferences.js
@@ -1,29 +1,28 @@
 // Theme: False = Light Mode; True = Dark Mode
 // Lang: False = Dutch; True = English
 const themeToggler = document.getElementById("toggle-theme");
-initializePage();
 let preferences = {
   theme: false,
-  lang: false
+  lang: false,
 };
-
+loadPreferences();
 
 themeToggler.addEventListener("change", () => {
   toggleTheme(themeToggler.checked);
   if (themeToggler.checked) {
     preferences.theme = true;
     localStorage.setItem("preferences", JSON.stringify(preferences));
-    console.log(localStorage.getItem("preferences"));
   } else {
     preferences.theme = false;
     localStorage.setItem("preferences", JSON.stringify(preferences));
-    console.log(localStorage.getItem("preferences"));
   }
 });
 
-function initializePage() {
-  let {theme, lang} = JSON.parse(localStorage.getItem("preferences"));
+function loadPreferences() {
+  let { theme, lang } = JSON.parse(localStorage.getItem("preferences"));
+  preferences = { theme, lang };
   themeToggler.checked = theme;
+  
   toggleTheme(theme);
   toggleLang(lang);
 }
@@ -42,6 +41,4 @@ function toggleTheme(theme) {
   }
 }
 
-function toggleLang(lang) {
-  
-}
+function toggleLang(lang) {}
